201832246947c8fd4128694d036961d69e7f2319
okayu <34333583+okayun@users.noreply.github.com>
2017-12-29 23:43:41 +0900

    Update template.cc

--- template/template.cc
+++ template/template.cc
@@ -1,12 +1,28 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
 
 using ll = long long;
 //typedef long long ll;
 
+using std::cin;
+using std::cout;
+using std::cerr;
+using std::endl;
+
 #define INF ((1LL << 31) - 1)
 #define INFLL ((1LL << 62) - 1)
-#define MOD int(1e9+7)
+#define MOD (ll)(1e9+7)
 #define repi(i,j,n) for(int i = (j); i < (n); ++i)
 #define rep(i,n) repi(i,0,n)
 #define rrep(i,n) for(int i = n; i >= 0; --i)
@@ -14,8 +30,8 @@ using ll = long long;
 #define se second
 #define all(v) (v).begin(), (v).end()
 
-// 4近傍のときは for(int i = 0; i < 8; i+=2) などとして使うこと
-int vx[8] = {0, 1, 1, 1, 0, -1, -1, -1}, vy[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
+int vx[4] = {-1, 0, 1, 0}, vy[4] = {0, -1, 0, 1};
+//int vx[8] = {0, 1, 1, 1, 0, -1, -1, -1}, vy[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
 
 // [sx, se), [sy, ey)であることに注意
 inline bool check(int sx, int ex, int sy, int ey, int x, int y) {
@@ -28,7 +44,7 @@ inline bool check(int ex, int ey, int x, int y) {
 
 inline void init() {
   cin.tie(0);
-  ios::sync_with_stdio(false);
+  std::ios::sync_with_stdio(false);
 }
 
 int main() {
