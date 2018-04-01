d5906b5b8f8ddc3ea04febcf2fe3e99d82725f59
okayu <34333583+okayun@users.noreply.github.com>
2018-04-01 23:11:11 +0900

    Update Combination_Permutation_memo.cc

--- numeric/Combination_Permutation_memo.cc
+++ numeric/Combination_Permutation_memo.cc
@@ -3,13 +3,13 @@
 using ll = long long;
 
 #define MOD (ll)(1e9 + 7)
-#define MAX_N 200000 // 適宜変更すること
+#define MAX_N 100001 // 適宜変更すること
 
 // memo
 ll fact[MAX_N], fact_inv[MAX_N];
 
 // n!
-void factorial_prepare (ll n) {
+void factorial_prepare(ll n) {
   fact[0] = fact[1] = 1;
   for (int i = 2; i <= n; ++i) {
     fact[i] = (i * fact[i - 1]) % MOD;
@@ -17,7 +17,7 @@ void factorial_prepare (ll n) {
 }
 
 // pow(x, n) := x^n
-ll power (ll x, ll n) {
+ll power(ll x, ll n) {
   if (n == 0) return 1LL;
 
   ll ret = power((x * x) % MOD, n / 2) % MOD;
@@ -28,7 +28,7 @@ ll power (ll x, ll n) {
 
 // x^(-1) ≡ x^(MOD - 2) (mod MOD)
 // (x!)^(-1) ≡ (x!)^(MOD - 2) (mod MOD)
-void inverse_prepare (ll x) {
+void inverse_prepare(ll x) {
   // (x!)^(-1)を求めて, その値にx, x-1, ... を順に掛ける
   fact_inv[x] = power(fact[x], MOD - 2);
 
@@ -38,13 +38,13 @@ void inverse_prepare (ll x) {
 }
 
 // nCr, nPr の想定される最大の n で初期化
-void init (ll n) {
+void init(ll n) {
   factorial_prepare(n);
   inverse_prepare(n);
 }
 
 // nCr
-ll C (ll n, ll r) {
+ll C(ll n, ll r) {
   if (n < r || r < 0) return 0LL;
   if (n == r || r == 0) return 1LL;
   // n! / ((n - r)! * r!)
@@ -52,9 +52,11 @@ ll C (ll n, ll r) {
 }
 
 // nPr
-ll P (ll n, ll r) {
+ll P(ll n, ll r) {
   if (n < r || r < 0) return 0LL;
-  return (fact[n] * fact_inv[r]) % MOD;
+  if (n == r) return (fact[n]) % MOD;
+  // n! / (n - r)!
+  return (fact[n] * fact_inv[n - r]) % MOD;
 }
 
 int main() {
